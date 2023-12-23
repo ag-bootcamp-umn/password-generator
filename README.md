dbd0d52d0701b4808c1bc33f38ce12201d49ca3e
alejandro <alexgeorgegeer@gmail.com>
2023-12-22 22:33:12 -0600

    Further refactored, separated functions, variables and logic

---
+++ README.md
@@ -0,0 +1,13 @@
+# Password Generator
+## Description
+This application will create a unique password for the user. The user is given a series of prompts to determine their desired password criteria, and a password is then generated in accordance to those criteria and then displayed on the screen. 
+
+## Logic
+The logic uses a series of prompts, functions and conditional statements to populate pre-declared variables to carry out its functionality.
+
+## Usage
+Usage of this app is simple; click on the "Generate Password" button, follow the prompts, and your new password will appear in the box marked "Your Secure Password." (See screenshot below.)
+
+![Screenshot of the app](<Screenshot 2023-12-22 222937.png>)
+
+## Links
--- script.js
+++ script.js
@@ -9,10 +9,14 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+
+// THE VARIABLES ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 
   var characters = [
     {
@@ -36,44 +40,75 @@ function generatePassword() {
   // Character set to be used to create password
   var charSet = "";
   var password = "";
+  var passLength = 0;
 
-  // prompt user for length
+  // THE FUNCTIONS ///////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////
 
-  var passLength = prompt(
-    "How long would you like your password to be? Please enter a number from 8 to 128."
-  );
-
-  if (passLength < 8 || passLength > 128) {
-    alert("Incorrect Length");
-    return generatePassword();
-  } else {
-
-    // prompt user for password character types and add those chosen to charSet string
-    function getUserCharTypes() {
-      for (var i = 0; i < characters.length; i++) {
-        var currentType = characters[i];
-        var confirmation = confirm(
-          "Would you like to use " + currentType.type + " characters?"
-        );
-        if (confirmation) {
-          charSet += currentType.set;
-        }
-      }
-
-      if (charSet === '') {
-        alert('Please choose at least one character type');
-        getUserCharTypes();
-      }
+    // prompt user for length
+    function getUserPassLength() {
+      passLength = prompt(
+        "How many characters would you like your password to be? Please enter a number from 8 to 128."
+      ) || 0;
     }
 
-    getUserCharTypes();
-    // Select characters randomly from charSet string
+  // prompt user for password character types and add those chosen to charSet string
+  function getUserCharTypes() {
+    for (var i = 0; i < characters.length; i++) {
+      var currentType = characters[i];
+      var confirmation = confirm(
+        "Would you like to use " + currentType.type + " characters?"
+      );
+      if (confirmation) {
+        charSet += currentType.set;
+      }
+    }
+    if (charSet === "") {
+      alert("Please choose at least one character type");
+      getUserCharTypes();
+    }
+  }
 
+  // Select characters randomly from charSet string
+  function createPassword() {
     for (var i = 0; i < passLength; i++) {
       var selection = Math.floor(Math.random() * charSet.length + 1);
       password += charSet.charAt(selection);
     }
-
-    return password;
   }
+
+  // THE LOGIC ///////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
+
+  // if ( passLength >= 8 && passLength <= 128) {
+  //   getUserCharTypes();
+  //   createPassword();
+  // } else if (passLength > 0) {
+  //       alert("Incorrect Length");
+  //   return generatePassword();
+  // } else {
+  //   console.log('passLength: ' + passLength);
+  //   console.log('password: ' + password);
+  // }
+
+  getUserPassLength();
+
+    if (!passLength) {
+    alert('Please enter a length for your password.')
+      return generatePassword();
+  } else if (passLength < 8 || passLength > 128) {
+    alert("Incorrect Length");
+    return generatePassword();
+  } else {
+    getUserCharTypes();
+    createPassword();
+    // return password;
+  }
+
+  if (password) {
+    return password;
+  } else {
+    return '';
+  }
+
 }
